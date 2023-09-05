ed6413935d9b13a0ddd37b7086c273767bbc2126
ClaraPlateiro <clara.plateiro@correo.ucu.edu.uy>
2023-09-05 19:50:24 -0300

    Agregar

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -7,11 +7,14 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand");
-            Console.WriteLine(appointmentResult);
+            Paciente paciente1 = new Paciente("Steven Jhonson", "5555-555-555", "986782342", "50");
+            Appoiments appoiments1 = new Appoiments(DateTime.Now, "Wall Street", "5555-555-555");
+            Doctor doctor1 = new Doctor("Armando", "Neurologo");
+            Validacion validacion1 = new Validacion(paciente1);
+            Validacion validacionn1 = new Validacion(appoiments1);
+            Validacion validacionnn1 = new Validacion(doctor1);
 
-            string appointmentResult2 = AppointmentService.CreateAppointment("Ralf Manson", "", "5555-555-555", DateTime.Now, "Queen Street", "");
-            Console.WriteLine(appointmentResult2);
+            Console.WriteLine($"Hello, {paciente1.Name}! Here is your medical appoinment:\n"+$"Pacient info:\nName: {paciente1.Name}\nID: {paciente1.ID}\nCell number: {paciente1.NumeroCel}\nAge: {paciente1.Age}\n"+$"Date:{appoiments1.Fecha}\n"+$"Place: {appoiments1.Lugar}\n"+$"Appoinment ID: {appoiments1.Codigo}\n"+$"You will be attended by {doctor1.DoctorName},{doctor1.Especialidad}");
         }
     }
 }
