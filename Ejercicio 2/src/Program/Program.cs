c2f6722fe7c338661947c85a891f43cd138391c1
ClaraPlateiro <clara.plateiro@correo.ucu.edu.uy>
2023-09-06 08:20:15 -0300

    Agregar

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -14,7 +14,7 @@ namespace Program
             Validacion validacionn1 = new Validacion(appoiments1);
             Validacion validacionnn1 = new Validacion(doctor1);
 
-            Console.WriteLine($"Hello, {paciente1.Name}! Here is your medical appoinment:\n"+$"Pacient info:\nName: {paciente1.Name}\nID: {paciente1.ID}\nCell number: {paciente1.NumeroCel}\nAge: {paciente1.Age}\n"+$"Date:{appoiments1.Fecha}\n"+$"Place: {appoiments1.Lugar}\n"+$"Appoinment ID: {appoiments1.Codigo}\n"+$"You will be attended by {doctor1.DoctorName},{doctor1.Especialidad}");
+            Console.WriteLine($"Hello, {paciente1.Name}! Here is your medical appoinment:\n"+$"Pacient info:\nName: {paciente1.Name}\nID: {paciente1.ID}\nCell number: {paciente1.NumeroCel}\nAge: {paciente1.Age}\n"+$"Dacdte:{appoiments1.Fecha}\n"+$"Place: {appoiments1.Lugar}\n"+$"Appoinment ID: {appoiments1.Codigo}\n"+$"You will be attended by {doctor1.DoctorName},{doctor1.Especialidad}");
         }
     }
 }
